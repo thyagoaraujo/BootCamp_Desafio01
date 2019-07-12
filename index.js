68da0a3855a249d66b168bd2840ffb2fc247df9d
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-07-12 17:16:50 -0300

    chore: Criado as rotas: create, edit, delete

--- index.js
+++ index.js
@@ -2,8 +2,38 @@ const express = require('express');
 
 const server = express();
 
-server.get('/teste', (req, res) => {
-  return res.json({ messenger: 'Hello World' });
+server.use(express.json());
+
+const projects = ['Projeto01', 'Projeto02'];
+//rota criar projeto
+
+server.post('/projects', (req, res) => {
+  const { title } = req.body;
+  projects.push(title);
+  return res.json(projects);
+});
+
+//rota listar projetos
+server.get('/projects/', (req, res) => {
+  return res.json(projects);
+});
+
+//rota editar titulo projeto
+server.put('/projects/:index', (req, res) => {
+  const { index } = req.params;
+  const { title } = req.body;
+
+  projects[index] = title;
+
+  return res.json(projects);
+});
+
+//rota deletar projeto
+server.delete('/projects/:index', (req, res) => {
+  const { index } = req.params;
+
+  projects.splice(index, 1);
+  return res.json({ message: 'Projeto deletado' });
 });
 
 server.listen(3000);
